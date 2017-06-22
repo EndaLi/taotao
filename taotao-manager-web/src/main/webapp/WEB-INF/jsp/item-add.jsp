c3d53d5c71ac03de8deccc2072c560245a17a4a9
EndaLi <894835707@qq.com>
2017-06-22 22:52:59 +0800

    实现商品的添加功能(富文本框的使用和FastDFS图片文件服务器的搭建)

--- taotao-manager-web/src/main/webapp/WEB-INF/jsp/item-add.jsp
+++ taotao-manager-web/src/main/webapp/WEB-INF/jsp/item-add.jsp
@@ -83,7 +83,7 @@
 			return ;
 		}
 		//取商品价格，单位为“分”
-		$("#itemAddForm [name=price]").val(eval($("#itemAddForm [name=priceView]").val()) * 100);
+		$("#itemAddForm [name=price]").val(eval($("#itemAddForm [name=priceView]").val()) * 1000);
 		//同步文本框中的商品描述
 		itemAddEditor.sync();
 		//取商品的规格
--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -1,5 +1,6 @@
 package com.taotao.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,22 +9,29 @@ import org.springframework.stereotype.Service;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.IDUtils;
+import com.taotao.mapper.TbItemDescMapper;
 import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemDesc;
 import com.taotao.pojo.TbItemExample;
 import com.taotao.service.ItemService;
 
 /**
  * 商品的service层
+ * 
  * @author Administrator
  *
  */
 @Service
-public class ItemServiceImpl implements ItemService{
+public class ItemServiceImpl implements ItemService {
 
 	@Autowired
 	private TbItemMapper itemMapper;
-	
+	@Autowired
+	private TbItemDescMapper itemDescMapper;
+
 	@Override
 	public TbItem getItemById(long itemId) {
 		TbItem item = itemMapper.selectByPrimaryKey(itemId);
@@ -32,19 +40,52 @@ public class ItemServiceImpl implements ItemService{
 
 	@Override
 	public EasyUIDataGridResult getItemList(int page, int rows) {
-		//设置分页信息
+		// 设置分页信息
 		PageHelper.startPage(page, rows);
-		//执行查询
+		// 执行查询
 		TbItemExample example = new TbItemExample();
 		List<TbItem> list = itemMapper.selectByExample(example);
-		//提取分页信息
+		// 提取分页信息
 		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
-		
-		//创建返回结果对象
+
+		// 创建返回结果对象
 		EasyUIDataGridResult result = new EasyUIDataGridResult();
-		result.setTotal((int)pageInfo.getTotal());
+		result.setTotal((int) pageInfo.getTotal());
 		result.setRows(list);
 		return result;
+	}
+
+	/**
+	 * 添加商品
+	 */
+	@Override
+	public TaotaoResult addItem(TbItem item, String desc) {
+		// 1、生成商品id
+		long itemId = IDUtils.genItemId();
+		// 2、补全TbItem对象的属性
+		item.setId(itemId);
+		// 商品状态，1-正常，2-下架，3-删除
+		item.setStatus((byte) 1);
+		Date date = new Date();
+		item.setCreated(date);
+		item.setUpdated(date);
+		//item.setImage("http://image.taotao.com/jd/2e45ff47f2e7424cb6d95fb9f05151bd.jpg");
+		// 3、向商品表插入数据
+		itemMapper.insert(item);
+		// 4、创建一个TbItemDesc对象
+		TbItemDesc itemDesc = new TbItemDesc();
+		
+		// 5、补全TbItemDesc的属性
+		itemDesc.setItemId(itemId);
+		itemDesc.setItemDesc(desc);
+		itemDesc.setCreated(date);
+		itemDesc.setUpdated(date);
+		
+		// 6、向商品描述表插入数据
+		itemDescMapper.insert(itemDesc);
+		
+		// 7、TaotaoResult.ok()
+		return TaotaoResult.ok();
 	}
 
 }
